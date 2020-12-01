13e9d34c8344120369ca82b9128fc847b3fd7ef3
Luis enrique Varela <lvarela@c1r7s6.42madrid.com>
2020-12-01 18:59:33 +0100

    probando

--- ft_get_resolution.c
+++ ft_get_resolution.c
@@ -6,7 +6,7 @@
 /*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/13 19:02:16 by lvarela           #+#    #+#             */
-/*   Updated: 2020/12/01 18:50:36 by lvarela          ###   ########.fr       */
+/*   Updated: 2020/12/01 18:59:15 by lvarela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ void				ft_get_resolution(t_scene **scene, char **line)
 	int				i;
 
 	resolution = (t_resolution *)malloc(sizeof(t_resolution) * 1);
-	i = 0;
+	i = 1;
 	while ((*line)[i] == ' ')
 		i++;
-	write(1, "vami1\n", 6);
 	resolution->x = ft_atoi(&(*line)[i]);
 	while ((*line)[i] >= '0' && (*line)[i] <= '9')
 		i++;
 	while ((*line)[i] == ' ')
 		i++;
-	write(1, "vami2\n", 6);
 	resolution->y = ft_atoi(&(*line)[i]);
 	(*scene)->resolution = resolution;
 	return ;
 }
+
